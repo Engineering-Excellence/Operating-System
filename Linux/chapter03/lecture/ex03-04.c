ba22df183e7fb4e1598c792192a55a934cdeccd2
Kyle <prophet@hanyang.ac.kr>
2024-02-24 00:23:35 +0900

    파일 다루기   - read, write

---
+++ Linux/chapter03/lecture/ex03-04.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(void) {
+    int fdin, fdout;
+    ssize_t nread;
+    char buffer[1024];
+
+    fdin = open("temp1.txt", O_RDONLY);
+    fdout = open("temp2.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);  // 새롭게 저장하려는 데이터가 기존 데이터보다 적을 경우 일부만 덮어쓰기 때문에 O_TRUNC 필요
+
+    /* 정상적으로 읽어 들인 내용이 1 바이트 이상인 동안 반복문 수행 */
+    while ((nread = read(fdin, buffer, 1024)) > 0) {
+        if (write(fdout, buffer, nread) < nread) {
+            /* write가 비정상적으로 수행됨(실패) */
+            fprintf(stderr, "write 실패\n");
+            close(fdin);
+            close(fdout);
+        }
+    }
+
+    /* 프로그램이 정상적으로 수행됨 */
+    close(fdin);
+    close(fdout);
+
+    return 0;
+}

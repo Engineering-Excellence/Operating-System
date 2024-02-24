3e9d8e7c9f4a8f3bda9b3b3bd9af57ab7d7e2ab8
Kyle <prophet@hanyang.ac.kr>
2024-02-24 12:27:54 +0900

    파일 다루기   - lseek

---
+++ Linux/chapter03/lecture/ex03-05.c
@@ -0,0 +1,17 @@
+//#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(void) {
+    int filedes;
+    off_t newpos;
+
+    filedes = open("data1.txt", O_RDONLY);
+
+    /* 읽기/쓰기 포인터를 EOF로 이동한다. */
+    newpos = lseek(filedes, (off_t) 0, SEEK_END);
+    printf("file size: %ld\n", newpos);
+
+    return 0;
+}

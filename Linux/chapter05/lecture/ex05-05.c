f7ec35823430cfece183d71f8ea0722c45ea5b92
Kyle <propheta@yonsei.ac.kr>
2024-03-10 21:55:51 +0900

    디렉터리 다루기   - rewinddir

---
+++ Linux/chapter05/lecture/ex05-05.c
@@ -0,0 +1,30 @@
+#include <dirent.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    DIR *dirp;
+    const struct dirent *dentry;
+
+    if ((dirp = opendir(".")) == NULL)
+        exit(1);
+
+    puts("존재하는 파일 목록");
+    while ((dentry = readdir(dirp)) != NULL) {
+        if (dentry->d_ino != 0)
+            puts(dentry->d_name);
+    }
+
+    rewinddir(dirp);    // 디렉터리 포인터가 가리키는 디렉터리 항을 첫 번째 항으로 초기화
+
+    puts("삭제된 파일 목록");
+    while ((dentry = readdir(dirp)) != NULL) {
+        if (dentry->d_ino == 0)
+            puts(dentry->d_name);
+    }
+
+    closedir(dirp);
+
+    return 0;
+}
